a6a33701261bbc18023f896aa1d6e500088c77ae
Rafael Guilherme <rafael_guilherme@hotmail.com>
2024-02-14 10:54:41 -0300

    feat: hero section

---
+++ src/components/CTAs.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<div class="flex items-center gap-4">
+    <button class={"specialBtn"}>
+        <p class="text-base sm:text-lg md:text-xl">Entre em contato</p>
+    </button>
+    <button class={"specialBtnDark"}>
+        <p class="text-base sm:text-lg md:text-xl">Comece &rarr;</p>
+    </button>
+</div>
